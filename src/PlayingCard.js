c3128eeae4028931dc652d0dff06d829c5b70786
Eddie Sandler <ed.sandler@gmail.com>
2024-04-08 12:47:54 -0400

    refactored with useAxios hook

--- src/PlayingCard.js
+++ src/PlayingCard.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import backOfCard from "./back.png";
 import "./PlayingCard.css"
-import useFlip from "./hooks";
+import {useFlip} from "./hooks";
 
 /* Renders a single playing card. */
 function PlayingCard({ front, back = backOfCard }) {
   const [isFacingUp, flipCard] = useFlip();
-  
+
 
   return (
     <img
--- src/hooks.js
+++ src/hooks.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
+import { v1 as uuid } from "uuid";
+import axios from "axios";
 
-function useFlip(currentValue = true) {
+ function useFlip(currentValue = true) {
   // call useState, "reserve piece of state"
   const [value, setValue] = useState(currentValue);
   const toggleCard = () => {
@@ -11,4 +13,16 @@ function useFlip(currentValue = true) {
   return [value, toggleCard];
 }
 
-export default useFlip;
+
+function useAxios() {
+  const [cards, setCards] = useState([]);
+
+  const addCard = async (url) => {
+    const response = await axios.get(url);
+    setCards(cards => [...cards, { ...response.data, id: uuid() }]);
+  };
+  return {cards,addCard}
+
+}
+
+export  {useFlip, useAxios}
